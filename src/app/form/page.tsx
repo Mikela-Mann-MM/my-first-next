7746e22debcc8b9b5a714ed55566e6ca32b67110
mikelamann1@gmail.com <mikelamann1@gmail.com>
2026-01-12 14:14:22 +0100

    validering med zod tilføjet

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,15 +1,50 @@
-
+"use client"
 
 import { saveMessage } from "../lib/actions";
+import { useActionState, useEffect, useState } from "react";
+import { z } from "zod";
 
-export default function FormPage() {
+export default function FormPage(){
+
+  const initialState = {
+    data: {
+      name: "",
+      message: ""
+    },
+    success: false
+  };
+
+   const [formState, formAction, isPending] = useActionState(saveMessage, initialState); //useActionState tager server action og initial state. Det er et hook fra React
   
+useEffect(function () {
+    console.log("Formular sendt og behandlet:", formState);
+}, [formState]);
+
+//noValidate forhindrer browserens egen validering html5, så vi kan håndtere det selv. 
     return (
-    <form action={saveMessage}>
-      <input name="message" placeholder="Skriv en besked" />
-      <button type="submit">Send</button>
+    <form action={formAction} noValidate> 
+      <label >Navn:
+        <input className={`bg-white text-black ${formState.error?.properties?.name && "border-red-500"}`} name="name" type="text" placeholder="Skriv dit navn" defaultValue={formState.data.name}/>
+      {formState.success ? null : <p>{ formState.error?.properties?.name?.errors }</p> }
+      </label>
+      <label >Besked:
+        <input name="message" type="text" placeholder="Skriv en besked" defaultValue={formState.data.message}/>
+       {formState.success ? null : <p>{ formState.error?.properties?.message?.errors }</p> } 
+      </label>  
+      
+      <button type="submit" disabled={isPending}>{isPending ? "Sender..." : "Send"}</button>
+      {formState.success ? <p>Tak for din tilmelding</p> : null }
     </form>
   );
 
 }
+
+/* <p>{ formState.error?.properties?.message.errors }</p> optional chaining?. ?. betyder:
+👉 “Hvis dette findes, så fortsæt — ellers stop og returnér undefined.” */
 /* Side med en form, der bruger en server action */
+
+/* Det hele er gemt i en serveraction, fordelen ved det er, 
+at vi gemmer koden væk fra brugeren, så de ikke kan se, hvad der sker omme bagved. 
+Det handler om sikkerhed. */
+
+/* Når FormAction svarer tilbage får vi et opdateret formState med det vi returnerer. isPending er en boolean - true/false */
--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -1,13 +1,68 @@
- 
-
-export async function saveMessage(formData: FormData) {
   'use server'  // Server action skal køre på serveren
-  const msg = formData.get("message"); //tidligere hvor vi ikke brugte next await e.formData()
-  const values = Object.fromEntries(formData)
+
+import { success, z } from "zod"; // til validering af data
+
+export async function saveMessage(prevState, formData ) {
+  
+  console.log("prevState i action:", prevState);
+
+/* const formData = new FormData();
+formData.append("name", "John Doe"); //dette er det der sker under kølerhjelmen */
+
+  const values = Object.fromEntries(formData) // fromData er et objekt med en masse entries
+
   console.log("konstrueret objekt:", values)
   console.log(values.name)
 
-  // valider med Zod!!
+  // valider med Zod!! - hvorfor valiedere vi en form.
+const schema = z.object({
+    name: z.string().min(2, "Navn skal være mindst 2 karakterer langt"),
+    //email: z.string().email("Ugyldig email adresse"),
+    message: z.string().min(5, "Besked skal være mindst 5 karakterer langt")
+  });
+
+  //1 trin i validering - vi starter med at lave et skema som bestemmer hvordan dataen skal se ud
+
+
+
+  const validated = schema.safeParse({
+    name: values.name,
+    message: values.message
+  })
+
+  //2 trin - vi sammenligner skemaet med de indtastede værdier.
+
+  if (!validated.success) {
+    return {
+      success: false, // validering fejlede
+      data: values, // tilføj de oprindelige værdier til validated objektet
+      error: z.treeifyError(validated.error) // omformater fejl objektet for nemmere brug på klienten
+    };
+  }
+
+  //3 trin kigge på det validerede objekt - hvis det fejler, så returner fejl til klienten
+
+  //tjek om data er ændret
+
+    //er der sket ændringer i formularen siden sidste submit?
+  if (values.name === prevState.data.name && values.message === prevState.data.message ) {
+    console.log("Ingen ændringer i data, spring lagring over");
+    return {
+      data: values,
+      success: true
+    }; // returner tidligere state uden ændringer
+  }
+
+  // Hvorfor validering?
+
+  //  - undgå brugerfejl - tastefejl. Alt bruger input skal altid valideres 
+  //  af 3 årsager: 1. bruger kan være ondsindet
+  // 2. hjælpe brugeren med at udfylde formen korrekt. 
+  // 3. data integritet - dataen skal gemmes et sted - måske sendes vha et post request til 
+  // et API - hvis dataen er forkert formateret kan det ødelægge databasen.
+  // - sikkerhed - undgå ondsindet kode ex ;DROP TABLE USERS; --
+
+  console.log("Kald til APIet");
 
   const res = await fetch("https://jsonplaceholder.typicode.com/users", {
     method: "POST",
@@ -15,8 +70,7 @@ export async function saveMessage(formData: FormData) {
       "Content-Type": "application/json"
     },
     // Auth token her hvis nødvendigt
-    body: JSON.stringify({ message: msg })
-    
+    body: JSON.stringify( validated.data)
   });
 
   if (!res.ok) {  
@@ -24,6 +78,7 @@ export async function saveMessage(formData: FormData) {
   }
 
   // redirect eller revalidate cache hvis nødvendigt
-  console.log("Besked modtaget:", msg);
-  console.log("Response:", res);
+  /* console.log("Besked modtaget:", msg); */
+  return validated; // returner success med de validerede data
+
 }
